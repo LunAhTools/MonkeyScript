06b7437172aee487d3db71b053d32827bae9938e
erictaso <erictaso@fonsen.com.tw>
2023-08-10 09:45:01 +0800

    add no youtube short

---
+++ NoYoutubeShort/index.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         no youtube short
+// @namespace    https://github.com/LunAhEric
+// @version      0.1
+// @description  土炮自動跳轉short成一般觀看影片
+// @author       LunAh
+// @match        https://www.youtube.com/shorts/*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com/
+// @grant        Copyright LunAh Eric
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const herf = window.location.href;
+    const url = herf.split('https://www.youtube.com/shorts/');
+    if(herf.match('shorts')){
+        const doman = 'https://www.youtube.com/watch?v=';
+        window.location = doman + url[1];
+    }
+})();
